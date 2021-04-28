899aeffba249d0b070b0047e1ef4553273541e2b
mgcarbonell <mgcarbonell@gmail.com>
2021-04-27 17:18:57 -0700

    Finishes Day 1 of 30 days of Python

---
+++ 01_numbers_operators.py
@@ -0,0 +1,16 @@
+# Exercises:
+# Print your age to the console
+print(34)
+
+# Calculate and print the number of days, weeks, and months in 27 years (Don't worry about leaps)
+days = 27 * 365
+print(days)
+weeks = 27 * 52
+print(weeks)
+months = 27 * 12
+print(months)
+
+# Calculate and print the area of a circle with a radius of 5 units
+# area = pi r 2
+area = 3.14 * (5**2)
+print(area)
